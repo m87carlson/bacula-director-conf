826688a9d2be5a66bbac95b7fd393f6467c17534
Mike Carlson <mike@bayphoto.com>
2012-09-20 14:21:40 -0700

    Fixed #3 : I'm now using the fd.conf file to control the bacula dir location, and where client conf files go. The global variables have been removed, and all of the functions have been updated to include a bacula_dir argument.

--- bin/bcreate-fd.py
+++ bin/bcreate-fd.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python
+
+# Importing standard python libraries
 import re
 import sys, os, os.path
-import string, random, uuid
+import string, random
 import ConfigParser
 import argparse
 
+# Importing 3rd party libraries
 try:
-        from OpenSSL import crypto
-except ImportError:
-        print >>sys.stderr, 'Import Error: py-openssl is missing, please install from your package provier.'
-        sys.exit(1)
+    from OpenSSL import crypto
+except:
+    print >>sys.stderr, 'Error: bcreate-fd requires OpenSSL, please install using "sudo pip install OpenSSL"'
+    sys.exit(1)
 
 try:
-        from couchdbkit import *
+    from couchdbkit import *
 except ImportError:
-        print >>sys.stderr, 'Import Error: bcreate-fd requires CouchDB Kit. Please install using "sudo pip install couchdbkit"'
-        sys.exit(1)
+    print >>sys.stderr, 'Error: bcreate-fd requires CouchDB Kit. Please install using "sudo pip install couchdbkit"'
+    sys.exit(1)
 
 try:
-        from jinja2 import Template, Environment, PackageLoader
+    from jinja2 import Template, Environment, PackageLoader
 except ImportError:
-        print >>sys.stderr, 'Import Error: bcreate-fd requires Jinja2 Templates. Please install using "sudo pip install jinja2"'
-        sys.exit(1)
+    print >>sys.stderr, 'Error: bcreate-fd requires Jinja2 Templates. Please install using "sudo pip install jinja2"'
+    sys.exit(1)
 
 # Global Defaults
-bdir            = '/etc/bacula/'
-bcert_dir       = bdir + 'certs/'
-couchdb_server  = 'https://couchdb.example.com/'
-couchdb_db      = 'bacula_meta'
+DEFAULT_CONFIG      = './fd.conf'
 
-def parse_schedules():
+def parse_schedules(bacula_dir):
         """
         Parse Schedules will look for a schedules.conf file ( by default, in /etc/bacula/ ), look for 'Name = ' and return an array.
 
@@ -50,172 +50,186 @@ def parse_schedules():
         """
         schedules = []
         try:
-                for line in open(bdir + "schedules.conf", "r"):
-                    if "Name" in line:
-                            schedules.append(line.strip().replace('"','').replace(' ','').split("=")[-1])
+            for line in open(bacula_dir + "/schedules.conf", "r"):
+                if "Name" in line:
+                    schedules.append(line.strip().replace('"','').replace(' ','').split("=")[-1])
         except:
-                print >>sys.stderr, 'Error: %sschedules.conf does not exist. Please create one.' % bdir
-                sys.exit(1)
+            print sys.stderr, 'Error: %sschedules.conf does not exist. Please create one.' % bacula_dir
+            sys.exit(1)
 
         return schedules
 
 def read_in_args_and_conf():
-        """
-        Using argparse, this function will parse the arguments passed to it, and use a few defaults from the fd.conf file.
-        """
-        def_conf = './fd.conf'
-        config = {}
-        schedule_choices = []
-        argp = argparse.ArgumentParser(
-                        description='Create a FD configuration file Bacula',
-                        add_help = True,
-                        usage = '%(prog)s [options]'
+        conf_parser = argparse.ArgumentParser(
+            add_help = False
         )
 
-        schedule_choices    = parse_schedules()
-
-        schedule_default    = 'Standard'
-        domain_choices      = [ 
-                                'example.org', 
-                                'example.com' 
-                              ]
-        domain_default      = 'example.com'
-        os_type_choices     = [ 
-                                'unix', 
-                                'win', 
-                                'osx' 
-                              ]
-        storage_node_choices= [ 'sd-1' ]
-
-        os_type_default     = 'unix'
-
-        argp.add_argument(
-                        '--client-conf-dir',
-                        help = 'Directory for client configuration files',
-                        dest = 'client_conf_dir',
-                        default = bdir + 'clients.d/'
+        conf_parser.add_argument(
+            "-c", "--config-file",
+            dest="configfile",
+            help="Use a different config file other than %s" % DEFAULT_CONFIG
         )
 
-        argp.add_argument(
-                        '-c', '--config',
-                        default = def_conf,
-                        dest = 'configfile',
-                        help = "Use a different configuration file other than %s" % def_conf
-        )
+        args, remaining_argv = conf_parser.parse_known_args()
 
-        argp.add_argument(
-                        '-s', '--schedule',
-                        default = schedule_default,
-                        choices = schedule_choices,
-                        help = 'Set a backup schedule for the client',
-        )
+        if args.configfile:
+            configfile = args.configfile
+        else:
+            configfile = DEFAULT_CONFIG
 
-        argp.add_argument(
-                        '-t', '--os-type',
-                        default = os_type_default,
-                        choices = os_type_choices,
-                        help = 'Client OS type',
-        )
-        
-        argp.add_argument(
-                        '-d', '--domain',
-                        default = domain_default,
-                        choices = domain_choices,
-                        help = 'Domain (ie bayphoto.com) that the client is under',
-        )
+        # Testing if the config file defined exists
+        if not os.path.isfile(configfile):
+            print >>sys.stderr, 'ERROR: %s is not a file' % configfile
 
-        argp.add_argument(
-                        '-H', '--hostname',
-                        help = 'Short hostname of fd client',
-                        required = True
-        )
-
-        argp.add_argument(
-                        '-n', '--storage-node',
-                        help = 'Bacula storage node location',
-                        choices = storage_node_choices,
-                        dest = 'storage_node',
-                        default = 'sd-1'
-        )
-
-        # Read in the arguments
-        args = argp.parse_args()
-
-        # Set up cfgfile parser
-        cfgp = ConfigParser.SafeConfigParser()
+        config = ConfigParser.SafeConfigParser()
         try:
-                cfgp.read(args.configfile)
+            config.read([configfile])
         except:
-                print >>sys.stderr, 'ERROR: There is an error in the config file: %s' % args.configfile
-                sys.exit(1)
+            print >>sys.stderr, 'ERROR: There is an error in the config file, %s' % configfile
+
+        defaults = dict(config.items("default"))
+
+        #Assing conf key values to default variables.
+        bacula_dir          = defaults["bacula_dir"]
+        client_conf_dir     = defaults["client_conf_dir"]
+
+        # We parse the currently configured schedules in the bacula_dir
+        schedule_choices    = parse_schedules(bacula_dir)
+        schedule_default    = defaults["schedule"]
+
+        domain_choices      = [
+                                defaults["domain"],
+                                'bayphoto.com'
+                              ]
+        domain_default      = defaults["domain"]
         
-        # Read all sections of the configfile into the config dictionary
-        for section in cfgp.sections():
-                config.update(dict(cfgp.items(section)))
+        os_type_default     = defaults["os_type"]
+        os_type_choices     = [
+                                defaults["os_type"],
+                                'win',
+                                'osx'
+                              ]
 
-        config.update(dict(args._get_kwargs()))
+        storage_node_default= defaults["storage_node"]
+        storage_node_choices= [
+                                defaults["storage_node"],
+                              ]
 
-        print "Adding host: %s" % config['hostname']
-        print """
-            FQDN:       %s
-            Schedule:   %s
-            OS:         %s
-        """ % ( config['hostname'] + "." + config['domain'], config['schedule'], config['os_type'] )
+        parser = argparse.ArgumentParser(
+            # Inherit options from config_parser
+            parents = [conf_parser],
+            description = __doc__,
+            formatter_class = argparse.RawDescriptionHelpFormatter,
+            add_help = True,
+            usage = '%(prog)s [options]'
+        )
 
-        return config
+        parser.set_defaults(**defaults)
 
-def write_fd_conf(hostname, schedule, fqdn, os_type, storage_node, passhash, client_dir=bdir + "client.d" ):
+        parser.add_argument(
+            '-H', '--hostname',
+            help = 'Short hostname of fd client',
+            required = True
+        )
+
+        parser.add_argument(
+            '-d', '--domain',
+            default = domain_default,
+            choices = domain_choices,
+            help    = 'Domain (ie: example.com) that the fd client is in'
+        )
+
+        parser.add_argument(
+            '-s', '--schedule',
+            default = schedule_default,
+            choices = schedule_choices,
+            help = 'Set a backup schedule for the client'
+        )
+
+        parser.add_argument(
+            '-t', '--os-type',
+            default = os_type_default,
+            choices = os_type_choices,
+            help = 'FD Client OS type'
+        )
+
+        parser.add_argument(
+            '-n', '--storage-node',
+            default = storage_node_default,
+            choices = storage_node_choices,
+            help = 'Bacula storage node'
+        )
+
+        parser.add_argument(
+            '--client-conf-dir',
+            default = client_conf_dir,
+            help = 'Override the default client configuration directory'
+        )
+
+        parser.add_argument(
+            '--bacula-dir',
+            default = bacula_dir,
+            help = 'Override the default bacula configuration directory'
+        )
+
+        # capture args
+        args = parser.parse_args(remaining_argv)
+
+        # make args into a dictionary
+        d = args.__dict__
+        print d['couchdb_server'] + d['couchdb_db']
+        return d
+
+def write_fd_conf(hostname, schedule, fqdn, os_type, storage_node, passhash, client_dir):
         # parse and build storage node string
-        #    ex:   SD1File244
+        #    ex:   SD1FileD244
         #            ^     ^
         #       SD Node   Random drive letter
-        node = 'SD' + storage_node.split('-')[-1] + 'File%s' % random.randint(1,512)
-        
+        node = 'SD' + storage_node.split('-')[-1] + 'FileD%s' % random.randint(1,512)
+
         # Load the bcreate jinja template environment
         env = Environment(loader=PackageLoader('bcreate-fd', 'templates'))
         # use template/fd.tpl
         template = env.get_template('fd.tpl')
-        # 
+
         f = open(client_dir + "/" + hostname + ".conf", "w")
         f.write( template.render(schedule=schedule, fqdn=fqdn, os_type=os_type, storage_node=node, passhash=passhash) )
         f.close()
 
-def get_record_from_couchdb(fd):
+def get_record_from_couchdb(couchdb_server, couchdb_db, fd):
         """
         Returns a couchdb document from the couchdb_db database for the document 'fd',
         the bacula client.
 
         Example document:
             {
-                "_id": "host-a",
-                "_rev":"2-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-                "hostname":"host-a",
-                "passhash":"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-                "_attachments": {
-                        "host-a.example.com-fd.pem": {
-                                "content_type":"application/octet-stream",
-                                "revpos":14,
-                                "digest":"md5-XXXXXXXXXXXXXXXXXXXXX==",
-                                "length":3399,
-                                "stub":true
-                         }
-                }
+                    "_id": "host-a",
+                    "_rev":"2-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                    "hostname":"host-a",
+                    "passhash":"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                    "_attachments": {
+                            "host-a.example.com-fd.pem": {
+                                    "content_type":"application/octet-stream",
+                                    "revpos":14,
+                                    "digest":"md5-XXXXXXXXXXXXXXXXXXXXX==",
+                                    "length":3399,
+                                    "stub":true
+                             }
+                    }
             }
-
         if a document does not exist, it will create a new one.
-
         """
         server = Server(uri=couchdb_server)
         db = server.get_db(couchdb_db)
         try:
-                db.get(fd)
+            db.get(fd)
         except:
-                print >>sys.stderr, "Client does not exist. A new record for %s will be created." % fd
-                create_new_couchdb_record(fd)
+            print >>sys.stderr, "Client does not exist. A new record for %s will be created." % fd
+            create_new_couchdb_record(couchdb_server, couchdb_db, fd)
 
         return db.get(fd)
 
-def create_new_couchdb_record(fd):
+def create_new_couchdb_record(couchdb_server, couchdb_db, fd):
         """
         Creates a new document in couchdb_db:
         Example document:
@@ -238,29 +252,27 @@ def generate_passhash():
         """
         return ''.join(random.choice(string.ascii_letters + string.digits) for x in range(32))
 
-def get_cert_from_couchdb(fd, domain):
-        server = Server(uri=couchdb_server)
-        db = server.get_db(couchdb_db)
+def get_cert_from_couchdb(couchdb_server, couchdb_db, fd, domain, bacula_dir):
+    server = Server(uri=couchdb_server)
+    db = server.get_db(couchdb_db)
 
-        cert_name = fd + '.' + domain + "-fd.pem"
-        try:
-            pem = db.fetch_attachment(fd, cert_name)
-        except:
-            print >>sys.stderr, '%s does not have a certificate in %s/%s. A new certificate will be generated.' % ( fd, couchdb_server, couchdb_db )
-            pem = generate_ssl_keypair(bcert_dir, fd + '.' + domain)
-            push_cert_to_couchdb(fd, domain, pem) 
+    cert_name = fd + '.' + domain + "-fd.pem"
+    try:
+        pem = db.fetch_attachment(fd, cert_name)
+    except:
+        print >>sys.stderr, '%s does not have a certificate in %s/%s. A new certificate will be generated.' % ( fd, couchdb_server, couchdb_db )
+        pem = generate_ssl_keypair(bacula_dir + "/certs/", fd + '.' + domain)
+        push_cert_to_couchdb(couchdb_server, couchdb_db, fd, domain, pem, bacula_dir + "/certs/")
 
-        return pem
-
-def push_cert_to_couchdb(fd,domain,pem):
+def push_cert_to_couchdb(couchdb_server, couchdb_db, fd, domain, pem, cert_dir):
         server = Server(uri=couchdb_server)
         db = server.get_db(couchdb_db)
         doc = db.get(fd)
 
         cert_name = fd + '.' + domain + "-fd.pem"
 
-        with open(bcert_dir + cert_name) as f:
-                db.put_attachment(doc,f,cert_name)
+        with open(cert_dir + cert_name) as f:
+            db.put_attachment(doc,f,cert_name)
 
         print 'certificate pushed to %s for %s' % ( couchdb_server + couchdb_db, fd )
 
@@ -283,16 +295,22 @@ def generate_ssl_keypair(cert_dir, fqdn, is_valid=True):
 
         # create a key pair
         key = crypto.PKey()
-        key.generate_key(crypto.TYPE_RSA, 2048)
+        key.generate_key(crypto.TYPE_RSA, 4096)
                                                                                
         # create a self-signed cert            
         cert = crypto.X509()
         cert.get_subject().C = 'US'
         cert.get_subject().ST = 'California'
         cert.get_subject().L = 'Santa Cruz'
-        cert.get_subject().O = 'Bay Photo Lab'
+        cert.get_subject().O = 'Your Company Name'
         cert.get_subject().OU = 'IT'
+        cert.get_subject().emailAddress = 'admin@example.com'
         cert.get_subject().CN = fqdn
+
+        # Add X509v3 Extension
+        ext1 = crypto.X509Extension('subjectKeyIdentifier', False, 'hash', subject=cert )
+        cert.add_extensions([ext1])
+
         cert.set_serial_number(1000)
         cert.gmtime_adj_notBefore(0)
         cert.gmtime_adj_notAfter(10 * 365 * 24 * 60 * 60)
@@ -315,33 +333,51 @@ def generate_ssl_keypair(cert_dir, fqdn, is_valid=True):
 def main():
         # Read in the command line arguments
         args        = read_in_args_and_conf()
-
-        # For readability, reassign the returned dictionary to fd_ variables.
-        #  fd = file deamon, bacula's terminology.
-        fd_hostname     = args['hostname']
-        fd_domain       = args['domain']
-        fd_schedule     = args['schedule']
+        bacula_dir      = args["bacula_dir"]
+        fd_hostname     = args["hostname"]
+        fd_domain       = args["domain"]
+        fd_schedule     = args["schedule"]
         fd_fqdn         = fd_hostname + "." + fd_domain
-        fd_os_type      = args['os_type']
-        fd_storage_node = args['storage_node']
-        fd_conf_dir     = args['client_conf_dir']
+        fd_os_type      = args["os_type"]
+        fd_storage_node = args["storage_node"]
+        fd_client_dir   = args["client_conf_dir"]
 
-        # Return the document from couchdb of the client
-        doc         = get_record_from_couchdb(fd_hostname)
+        couchdb_server  = args["couchdb_server"]
+        couchdb_db      = args["couchdb_db"]
+
+        # Return document from couchdb
+        doc             = get_record_from_couchdb(couchdb_server, couchdb_db, fd_hostname)
+
         # Set the retrieved password to fd_password
-        fd_passhash = doc['passhash']
+        fd_passhash     = doc['passhash']
 
         # Get a cert. If it does not exist, one will be created.
-        get_cert_from_couchdb(fd_hostname, fd_domain)
+        get_cert_from_couchdb(couchdb_server, couchdb_db, fd_hostname, fd_domain, bacula_dir)
 
-        # Write out the template to bdir
+        print "Adding host: %s" % fd_hostname
+
+        print """
+        hostname:   %s
+        domain:     %s
+        schedule:   %s
+        fqdn:       %s
+        os_type:    %s
+        storage:    %s
+        client.d:   %s
+        passhash:   %s
+
+        couch_server: %s
+        couch_db: %s
+        """ % ( fd_hostname, fd_domain, fd_schedule, fd_fqdn, fd_os_type, fd_storage_node, fd_client_dir, fd_passhash, couchdb_server, couchdb_db )
+
+        # Write out the template to client_conf_dir
         write_fd_conf(
-                        fd_hostname, fd_schedule,
-                        fd_fqdn, fd_os_type,
-                        fd_storage_node, fd_passhash,
-                        fd_conf_dir
+            fd_hostname, fd_schedule,
+            fd_fqdn, fd_os_type,
+            fd_storage_node, fd_passhash,
+            fd_client_dir
         )
-        
+
         sys.exit(0)
 
 if __name__ == '__main__':
